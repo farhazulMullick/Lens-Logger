2194129b7e66282485f48aaf7c767cfc4e191772
Mullick <farhazulmullick99@gmail.com>
2025-06-15 13:20:40 +0530

    Added expandable cards for headers and response body.

--- composeApp/src/commonMain/kotlin/io/github/farhazulmullick/ui/NetLoggingInfoScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/farhazulmullick/ui/NetLoggingInfoScreen.kt
@@ -1,7 +1,11 @@
 package io.github.farhazulmullick.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,19 +16,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import io.github.farhazulmullick.lensktor.modal.NetworkLogs
 import io.github.farhazulmullick.lensktor.modal.Resource
@@ -136,21 +150,92 @@ fun ResponsePageUI(
                     VSpacer(12.dp)
 
                     // request header
-                    Text(text = "Response headers ->")
-                    Text("{")
-                    it.headers?.forEach { entry ->
-                        Text("  ${entry.key} : ${entry.value}")
-                    }
-                    Text("}")
+                    var isRequestExpanded by remember { mutableStateOf(false) }
+                    ExpandableCard(
+                        title = "Response Headers",
+                        isExpanded = isRequestExpanded,
+                        onClick = {
+                            isRequestExpanded = !isRequestExpanded
+                        },
+                        content = {
+                            SelectionContainer {
+                                Column (Modifier.fillMaxWidth()){
+                                    VSpacer(16.dp)
+                                    it.headers?.forEach { entry ->
+                                        Text(text = "${entry.key} : ${entry.value}",
+                                            color = MaterialTheme.colorScheme.onSurface,
+                                            style = MaterialTheme.typography.bodyLarge
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    )
+
+                    VSpacer(12.dp)
                     // body
-                    Text(text = "Response Body ->")
-                    Text(it.body ?: "No body found")
+                    var isResponseExpanded by remember { mutableStateOf(true) }
+                    ExpandableCard(
+                        title = "Body",
+                        isExpanded = isResponseExpanded,
+                        onClick = {
+                            isResponseExpanded = !isResponseExpanded
+                        },
+                        content = {
+                            SelectionContainer {
+                                Column (Modifier.fillMaxWidth()){
+                                    VSpacer(16.dp)
+                                    Text(
+                                        modifier = Modifier.animateContentSize(), text = it.body ?: "No body found",
+                                        color = MaterialTheme.colorScheme.onSurface
+                                    )
+                                }
+                            }
+                        }
+                    )
                 }
             }
             is Resource.Failed -> {}
-
             else -> {}
 
+        }
+    }
+}
+
+@Composable
+fun ExpandableCard(
+    title: String,
+    modifier: Modifier = Modifier,
+    isExpanded: Boolean = false,
+    onClick: () -> Unit = {},
+    content: @Composable () -> Unit,
+){
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .clip(shape = RoundedCornerShape(8.dp))
+            .background(color = MaterialTheme.colorScheme.surfaceVariant)
+            .padding(16.dp)
+            .then(modifier)
+    ) {
+        Row (modifier = Modifier.fillMaxWidth()
+            .clickable { onClick() },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            Icon(imageVector = if (!isExpanded) Icons.Outlined.KeyboardArrowDown
+            else Icons.Outlined.KeyboardArrowUp,
+                contentDescription = null,
+                modifier = Modifier.clickable { onClick() }
+            )
+        }
+        AnimatedVisibility(isExpanded) {
+            content()
         }
     }
 }
